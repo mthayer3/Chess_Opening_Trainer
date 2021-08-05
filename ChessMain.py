dd8c773905847a2d1a334c5f0ed434fa631a70fe
Matthew Thayer <38388385+mthayer3@users.noreply.github.com>
2021-08-05 00:39:23 -0700

    Display Board and pieces

--- ChessMain.py
+++ ChessMain.py
@@ -4,7 +4,7 @@ Responsible for handling user input and displaying current game state object
 """
 
 import pygame as p
-from chess_opening_trainer import ChessEngine
+import ChessEngine
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8 #Dimensions of a chess board are 8x8
@@ -29,8 +29,51 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(gs.board)
+    loadImages() #Do before while loop to ensure it is only done once
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
+"""
+Responsible for all graphics within current game state
+"""
+def drawGameState(screen, gs):
+    drawBoard(screen) #Draw squares on the board
+    drawPieces(screen, gs.board) #Draw pieces on top of the squares
+
+"""
+Draw the squares on the board. Top left square is always light
+"""
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    
+
+"""
+Draw the pieces on top of the board
+"""
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #Not empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+
+if __name__ == "__main":
+    main()
+
+main()
 
 
 
