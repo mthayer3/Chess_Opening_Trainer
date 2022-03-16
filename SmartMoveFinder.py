ca5737a51ace0d4844d1c72ef3993dadd75cb941
Matthew Thayer <38388385+mthayer3@users.noreply.github.com>
2022-03-15 19:41:40 -0700

    Scotch gambit random move assignment

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -4,12 +4,27 @@ import ChessEngine
 
 
 def findRandomMove(validMoves, currBoard):
-    if currBoard in my_dict.values():
-        print("YOOOOOOOOOOOUUUUUUU")
-        for key, value in my_dict.items():
-            if value == currBoard:
-                print(key)
-        return ChessEngine.Move((1,4), (3,4), currBoard)
+    # if currBoard in my_dict.values():
+    #     print("YOOOOOOOOOOOUUUUUUU")
+    for key, value in my_dict.items():
+        if value == currBoard:
+            
+            chance = random.randint(1,100)
+            print(chance)
+            print(key)
+            if len(key)>=2:
+
+                for i in key:
+                    if chance < i[-1]:
+                        return ChessEngine.Move((i[0],i[1]), (i[2], i[3]), currBoard)
+            else:
+                if chance < key[-1]:
+                        return ChessEngine.Move((key[0],key[1]), (key[2], key[3]), currBoard)
+                
+                        
+
+                # return ChessEngine.Move()
+        # return ChessEngine.Move((1,4), (3,4), currBoard)
         
     else:
         return validMoves[random.randint(0, len(validMoves)-1)]
